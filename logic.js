411e9e7c2ecb8828e6eb6df1ddaa26dbec6696cd
aanchal <259937850+aanchal-devspace@users.noreply.github.com>
2026-07-05 10:04:43 +0530

    made second web page

--- logic.js
+++ logic.js
@@ -25,6 +25,66 @@ people:people
 
 localStorage.setItem("trip",JSON.stringify(trip));
 
-window.location="planner.html";
+window.location="plan.html";
 
+}
+let act=[];
+
+function loadTrip(){
+let trip=JSON.parse(localStorage.getItem("trip"));
+document.getElementById("city").innerHTML=trip.city;
+document.getElementById("start").innerHTML=trip.start;
+document.getElementById("end").innerHTML=trip.end;
+document.getElementById("budget").innerHTML=trip.budget;
+document.getElementById("people").innerHTML=trip.people;
+}
+
+function add(){
+let txt=document.getElementById("txt").value;
+
+if(txt==""){
+alert("Enter activity");
+return;
+}
+act.push(txt);
+
+show();
+document.getElementById("txt").value="";
+
+let p=act.length*20;
+if(p>100){
+p=100;
+}
+
+document.getElementById("fill").style.width=p+"%";
+document.getElementById("per").innerHTML=p+"% Completed";
+if(act.length>=5){
+document.getElementById("next").disabled=false;
+}
+}
+function show(){
+let list=document.getElementById("list");
+
+list.innerHTML="";
+for(let i=0;i<act.length;i++){
+list.innerHTML+=`
+<li>
+${act[i]}
+<button onclick="del(${i})">Delete</button>
+</li>
+`;
+}
+
+}
+function del(i){
+
+act.splice(i,1);
+show();
+let p=act.length*20;
+
+document.getElementById("fill").style.width=p+"%";
+document.getElementById("per").innerHTML=p+"% Completed";
+if(act.length<5){
+document.getElementById("next").disabled=true;
+}
 }
